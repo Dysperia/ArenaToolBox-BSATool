4210f53ca1c416bb1e13ad5a47477a0120db03a1
david aussourd <softwatermermaid@hotmail.fr>
2018-03-03 21:30:43 +0100

    added design pattern : template singleton

---
+++ src/designpatterns/Singleton.h
@@ -0,0 +1,72 @@
+#ifndef SINGLETON_H
+#define SINGLETON_H
+
+#include <memory>
+
+/**
+ * @Brief Design pattern singleton
+ *
+ * The current design requires that the implementation declare only one
+ * constructor taking only a token parameter (this ensures that no instance are
+ * possible outside the singleton)
+ */
+template <typename T> class Singleton
+{
+public:
+    //**************************************************************************
+    // Constructors
+    //**************************************************************************
+    /**
+     * @brief copy constructor : forbidden to use
+     * @param singleton singleton to copy
+     */
+    Singleton(const Singleton &singleton) = delete;
+
+    //**************************************************************************
+    // Methods
+    //**************************************************************************
+    /**
+     * @brief operator = assignment
+     * @param singleton to copy
+     * @return nothing : forbidden to use
+     */
+    T &operator=(const T &singleton) = delete;
+    /**
+     * @brief Allowing to retrieve the only instance of the class
+     * @return the only instance
+     */
+    static T& getInstance();
+
+protected:
+    //**************************************************************************
+    // Constructors/Destructor
+    //**************************************************************************
+    /**
+     * @brief Singleton constructor
+     */
+    Singleton() {}
+    /**
+     * @brief Destructor
+     */
+    virtual ~Singleton() {}
+
+    //**************************************************************************
+    // Attributes
+    //**************************************************************************
+    /**
+     * @brief Ensure that no instance are possible outside the singleton scope
+     */
+    struct token {};
+};
+
+//**************************************************************************
+// Methods
+//**************************************************************************
+template<typename T>
+T &Singleton<T>::getInstance()
+{
+    static T instance(token{});
+    return instance;
+}
+
+#endif // SINGLETON_H
