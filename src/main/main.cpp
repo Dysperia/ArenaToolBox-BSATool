e4997aaa1f5704d779b586830738a83b12452ba6
david aussourd <softwatermermaid@hotmail.fr>
2018-03-03 21:32:23 +0100

    added logs

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,10 +1,12 @@
 #include "../components/MainWindow.h"
 #include <QApplication>
+#include "../log/Logger.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     MainWindow w;
+    Logger::getInstance().log(Logger::MessageType::INFO, "Starting application");
     w.show();
     return app.exec();
 }
