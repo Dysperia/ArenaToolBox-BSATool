8f462cdc5ee71889b7550b8417b306b4d82e98e7
dysperia <softwatermermaid@hotmail.fr>
2019-10-20 15:50:11 +0200

    palette constructor from colors description

--- src/assets/Palette.cpp
+++ src/assets/Palette.cpp
@@ -1,9 +1,28 @@
+#include <QtCore/QDataStream>
+#include <error/Status.h>
 #include "Palette.h"
 
 Palette::Palette()
 {
     for (int i=0; i<256; i++) {
         mColorTable.append(qRgb(i,i,i));
+    }
+}
+
+Palette::Palette(const QVector<char> &rgbs, bool sixBitsColor) {
+    if (rgbs.size() != 768) {
+        throw Status(-1, QStringLiteral("Palette description does not contain 256 colors"));
+    }
+    QDataStream imgDataStream(QByteArray((rgbs.constData()), rgbs.size()));
+    quint8 r, g, b;
+    for (int i=0; i<256; i++) {
+        imgDataStream >> r >> g >> b;
+        if (sixBitsColor) {
+            r = r << 2u;
+            g = g << 2u;
+            b = b << 2u;
+        }
+        mColorTable.append(qRgb(r, g, b));
     }
 }
 
--- src/assets/Palette.h
+++ src/assets/Palette.h
@@ -16,6 +16,12 @@ public:
      * with colors from (R=1, G=1, B=1, A=255) to (R=255, G=255, B=255, A=255)
      */
     Palette();
+    /**
+     * @brief Palette constructor. Extract a palette from the given data
+     * @param rgbs palette description given as 256 RGB triplet (hence 768 length vector)
+     * @param sixBitsColor colors are expected to be coded on 6 bits if true, 8 bits otherwise
+     */
+    explicit Palette(const QVector<char> &rgbs, bool sixBitsColor = true);
 
     //**************************************************************************
     // Getters/setters
