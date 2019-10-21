67fb47e5cd4243348b56a7239abc097e28b6511e
Dysperia <softwatermermaid@hotmail.fr>
2019-10-21 14:35:06 +0200

    delete unused files

--- src/globalImage.cpp
+++
@@ -1,3 +0,0 @@
-#include "globalImage.hpp"
-
-Image image;
--- src/globalImage.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBALIMAGE_HPP
-#define GLOBALIMAGE_HPP
-
-#include "image.hpp"
-
-// Used to share a single instance among the project
-extern Image image;
-
-#endif // GLOBALIMAGE_HPP
